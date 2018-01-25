9bbebe34b03fb58b335ab00327e2788b3c1a2b6a
urooj1101 <urooj@unanth.com>
2018-01-25 15:36:17 +0530

    created home.php file created form.css file created form.js file

---
+++ Home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title> <!-- Include CSS file here -->
+<link href="form.css" rel="stylesheet">
+</head>
+<body>
+<div id="main"> 
+<h1>Login Page</h1>
+<div id="form_sample"></div> <!-- Include JS file here -->
+<script src="form.js"></script>
+</div>
+</body>
+</html>
